47c54865e64b118eda29426fc43b7430c438dd78
SVEND-1 <s5090@inbox.ru>
2026-05-04 16:35:59 +0300

    init

---
+++ src/main/java/org/example/culturetest/CultureTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.culturetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CultureTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/culturetest/CultureTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.culturetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
